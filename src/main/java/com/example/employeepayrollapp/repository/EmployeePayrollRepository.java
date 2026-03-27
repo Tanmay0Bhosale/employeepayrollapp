1f43038af5d6b785b4f5875e4398c7eba3440b17
Tanmay <tanmaytanvi1234@gmail.com>
2026-03-27 10:49:19 +0530

    [Tanmay] : Added UC7 - JPA Repository layer

--- src/main/java/com/example/employeepayrollapp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/example/employeepayrollapp/repository/EmployeePayrollRepository.java
@@ -2,6 +2,9 @@ package com.example.employeepayrollapp.repository;
 
 import com.example.employeepayrollapp.model.EmployeePayrollData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface EmployeePayrollRepository extends JpaRepository<EmployeePayrollData, Integer> {
+@Repository
+public interface EmployeePayrollRepository
+        extends JpaRepository<EmployeePayrollData, Integer> {
 }
